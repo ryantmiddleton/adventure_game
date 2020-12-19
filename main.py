71dc53caf116124d0a9d99b8ac4cb10c52866632
Michael.Erickson <merickson51293@gmail.com>
2020-12-19 15:47:10 -0500

    added platforms, just messing around with it

--- main.py
+++ main.py
@@ -31,13 +31,24 @@ class Game:
     self.player = Player(self)
     self.all_sprites.add(self.player)
     # ground platform
-    p1 = Platform(0, HEIGHT-40, WIDTH, 40)
+    p1 = Platform(0, HEIGHT -40, WIDTH, 40)
     self.all_sprites.add(p1)
     self.platforms.add(p1)
     # raised platform
-    p2 = Platform(WIDTH/2 - 50, HEIGHT * 3/4, 100, 20)
+    p2 = Platform(WIDTH/2 - 50, HEIGHT * 3/4, 100, 40)
     self.all_sprites.add(p2)
     self.platforms.add(p2)
+    # 2nd raised platform
+    p3 = Platform(WIDTH/4 - 50, HEIGHT * 3/4, 100, 40)
+    self.all_sprites.add(p3)
+    self.platforms.add(p3)
+    # 3rd raised platform
+    p4 = Platform(WIDTH - 100, HEIGHT * 3/4, 100, 20)
+    self.all_sprites.add(p4)
+    self.platforms.add(p4)
+    p5 = Platform(WIDTH - 100, HEIGHT * 3/4, 100, 40)
+    self.all_sprites.add(p5)
+    self.platforms.add(p5)
     self.run()
     
 
