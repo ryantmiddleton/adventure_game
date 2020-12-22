871f775e6f1f066687d82245a0fd81162d63d198
Michael.Erickson <merickson51293@gmail.com>
2020-12-21 19:49:01 -0500

    added all the maps, they need to be edited but they are added in settings

--- settings.py
+++ settings.py
@@ -18,6 +18,30 @@ MAP1_PLATFORM_LIST = [
   (175, 100, 50, 20)
 ]
 
+MAP2_PLATFORM_LIST = [
+  (0, HEIGHT - 40, WIDTH, 40),
+  (WIDTH/2 - 50, HEIGHT * 3/4, 100, 20),
+  (125, HEIGHT - 350, 100, 20),
+  (350, 200, 100, 20),
+  (175, 100, 50, 20)
+]
+
+MAP3_PLATFORM_LIST = [
+  (0, HEIGHT - 40, WIDTH, 40),
+  (WIDTH/2 - 50, HEIGHT * 3/4, 100, 20),
+  (125, HEIGHT - 350, 100, 20),
+  (350, 200, 100, 20),
+  (175, 100, 50, 20)
+]
+
+MAP4_PLATFORM_LIST = [
+  (0, HEIGHT - 40, WIDTH, 40),
+  (WIDTH/2 - 50, HEIGHT * 3/4, 100, 20),
+  (125, HEIGHT - 350, 100, 20),
+  (350, 200, 100, 20),
+  (175, 100, 50, 20)
+]
+
 # define colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
